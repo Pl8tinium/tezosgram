d728e9b7d24bd2b6d1e36e6b9f7aa863a11db10a
Pl8tinium <50824212+Pl8tinium@users.noreply.github.com>
2021-07-29 20:45:38 +0200

    forgot models

---
+++ src/app/services/boardService/board.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Dimension } from 'src/app/models/dimension';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BoardService {
+
+  public boardSize: Dimension;
+
+  constructor() { }
+}
