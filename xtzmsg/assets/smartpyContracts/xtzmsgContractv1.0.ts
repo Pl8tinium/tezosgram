4ec373475ec3d8db202c769d824fac5e28413233
Pl8tinium <50824212+Pl8tinium@users.noreply.github.com>
2021-09-30 22:54:22 +0200

    Auto-generated commit [ci skip]

---
+++ xtzmsg/assets/smartpyContracts/xtzmsgContractv1.0.ts
@@ -0,0 +1,29 @@
+export class XtzMsgOnChainContract {
+    public static currentVersion: object[] = [
+        { "prim": "storage", "args": [{ "prim": "pair", "args": [{ "prim": "string", "annots": ["%channelName"] }, { "prim": "string", "annots": ["%lastMsg"] }] }] },
+        { "prim": "parameter", "args": [{ "prim": "string", "annots": ["%addMsg"] }] },
+        {
+            "prim": "code",
+            "args": [[{ "prim": "UNPAIR" }, { "prim": "UPDATE", "args": [{ "int": "2" }] }, { "prim": "NIL", "args": [{ "prim": "operation" }] }, { "prim": "PAIR" }]]
+        }
+    ];
+}
+
+// import smartpy as sp
+
+// class Xtzmsg(sp.Contract):
+//     def __init__(self, channelN):
+//         self.init(channelName = channelN, lastMsg = "")
+
+//     @sp.entry_point
+//     def addMsg(self, msg):
+//         self.data.lastMsg = msg
+
+// if "templates" not in __name__:
+//     @sp.add_test(name = "Test messages")
+//     def test():
+//         scenario = sp.test_scenario()
+//         c1 = Xtzmsg("Test channel name")
+//         scenario += c1
+//         scenario += c1.addMsg("A Message")
+//         scenario += c1.addMsg("Another onse")
---
+++ xtzmsg/assets/templateStorage.ts
@@ -0,0 +1,14 @@
+export class TemplateStorage {
+    public static readonly allowedContractsHashes: any = {
+        "3c3956f3": "v0.1",
+    };
+    public static readonly trustedContracts: Array<string> = [
+        'KT1AD55utWAC27ubY2rQaYXDa7Vi37Ev9vHc',
+        'PredefinedContractAddr2',
+        'PredefinedContractAddr3'
+    ];
+    public static readonly predefinedRpcs: object = {
+        "Tezos Giganode": "https://mainnet-tezos.giganode.io",
+        "Tz stats": "https://rpc.tzstats.com"
+    };
+}
